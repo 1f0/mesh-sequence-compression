5906698492052ffd6cf978d2fc183d3a5bbee170
mlenta <strain_tea@163.com>
2016-12-28 19:25:53 +0800

    finish and make pack and unpack exe. Qbit can change precision. now it is 15.

--- main.cpp
+++ main.cpp
@@ -5,53 +5,52 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-//    if (argc != 3) {
-//        cout << "Usage: " << argv[0]
-//             << " Input_file[input.p3d] Output_file_Prefix[output]" << endl;
-//        exit(1);
-//    }
-//
-//    const char *input_file_name = argv[1];
-//    const char *output_file_name = argv[2];
-//
-//    PolyhedronPtr mesh_ptr(new Polyhedron());
-//
-//    //Decompression
-//    Compression_Valence_Component cv(mesh_ptr);
-//    cv.File_name = string(input_file_name);
-//    cout << (cv.Decompress_Init(*mesh_ptr)).toStdString() << endl;
-//
-//    while (cv.Current_level < cv.Total_layer) {
-//        cv.Current_level = cv.Decompress_Each_Step(*mesh_ptr, cv.File_name.c_str());
-//    }
-//    string s(output_file_name);
-//    char tmp[255];
-//    sprintf(tmp, "_lv%d.obj", cv.Current_level);
-//    s += string(tmp);
-//
-//    ofstream ofs(s.c_str(), ofstream::out);
-//    size_t index = 0;
-//    for (Vertex_iterator i = mesh_ptr->vertices_begin(); i != mesh_ptr->vertices_end(); ++i) {
-//        ofs << "v "
-//            << i->point().x() << " "
-//            << i->point().y() << " "
-//            << i->point().z() << endl;
-//        i->tag(index++);
-//    }
-//
-//    for (Facet_iterator i = mesh_ptr->facets_begin(); i != mesh_ptr->facets_end(); ++i) {
-//        ofs << "f";
-//        Halfedge_around_facet_circulator pHalfedge = i->facet_begin();
-//        do
-//            ofs << ' ' << pHalfedge->vertex()->tag() + 1;
-//        while (++pHalfedge != i->facet_begin());
-//        ofs << endl;
-//    }
-//
-//    ofs.close();
+    if (0) {
+        if (argc != 3) {
+            cout << "Usage: " << argv[0]
+                 << " Input_file[input.p3d] Output_file_Prefix[output]" << endl;
+            exit(1);
+        }
 
+        const char *input_file_name = argv[1];
+        const char *output_file_name = argv[2];
 
-    if (1) {// compression
+        PolyhedronPtr mesh_ptr(new Polyhedron());
+
+        //Decompression
+        Compression_Valence_Component cv(mesh_ptr);
+        cv.File_name = string(input_file_name);
+        cout << (cv.Decompress_Init(*mesh_ptr)).toStdString() << endl;
+
+        while (cv.Current_level < cv.Total_layer) {
+            cv.Current_level = cv.Decompress_Each_Step(*mesh_ptr, cv.File_name.c_str());
+        }
+        string s(output_file_name);
+        char tmp[255];
+        sprintf(tmp, "_lv%d.obj", cv.Current_level);
+        s += string(tmp);
+
+        ofstream ofs(s.c_str(), ofstream::out);
+        size_t index = 0;
+        for (Vertex_iterator i = mesh_ptr->vertices_begin(); i != mesh_ptr->vertices_end(); ++i) {
+            ofs << "v "
+                << i->point().x() << " "
+                << i->point().y() << " "
+                << i->point().z() << endl;
+            i->tag(index++);
+        }
+
+        for (Facet_iterator i = mesh_ptr->facets_begin(); i != mesh_ptr->facets_end(); ++i) {
+            ofs << "f";
+            Halfedge_around_facet_circulator pHalfedge = i->facet_begin();
+            do
+                ofs << ' ' << pHalfedge->vertex()->tag() + 1;
+            while (++pHalfedge != i->facet_begin());
+            ofs << endl;
+        }
+
+        ofs.close();
+    } else { // compression
         if (argc != 4) {
             cout << "Usage: " << argv[0]
                  << " Input_file[obj] Output_file[p3d] Remain_V_Num" << endl;
@@ -72,7 +71,7 @@ int main(int argc, char **argv) {
         int forget_value = 0;
         bool is_compression_selected = true;
         bool is_adaptive_quantization_selected = false;
-        bool is_bijection_selected = false;
+        bool is_bijection_selected = true;
 
         PolyhedronPtr mesh_ptr(new Polyhedron());
         mesh_ptr->load_mesh_obj(input_file_name);
